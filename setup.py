28a22c5ae9d8be03c6f1dbe7962aa7b75cd2acfc
Gert Kanter <gert.kanter@robokratt.xyz>
2019-02-04 17:21:37 +0200

    fix package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup()
-d['packages'] = ['testit']
+d['packages'] = ['testit_sut']
 d['package_dir'] = {'': 'src'}
 
 setup(**d)
